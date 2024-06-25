bb8ace8b4c8e56efac40886d30771395590a53b8
RonJoy22 <r.brownjoyner@gmail.com>
2024-06-25 09:38:45 -0400

    partially done

---
+++ app.js
@@ -0,0 +1,98 @@
+const adventurer = {
+    name: "Robin",
+    health: 10,
+    inventory: ["sword", "potion", "artifact"],
+    companion: {
+      name: "Leo",
+      type: "Cat",
+      companion: {
+        name: "Frank",
+        type: "Flea",
+      },
+    },
+    roll(mod = 0) {
+      const result = Math.floor(Math.random() * 20) + 1 + mod;
+      console.log(`${this.name} rolled a ${result}.`);
+    },
+  };
+  
+  //Class Fantasy
+  class Character {
+    constructor(name) {
+      this.name = name;
+      this.health = 100;
+      this.inventory = [];
+    }
+  
+    roll(mod = 0) {
+      const result = Math.floor(Math.random() * 20) + 1 + mod;
+      console.log(`${this.name} rolled a ${result}.`);
+      return result;
+    }
+  }
+  
+  //Class Features
+  class Adventurer extends Character {
+    static MAX_HEALTH = 100;
+    static ROLES = ["Fighter", "Healer", "Wizard"];
+    constructor(name, role) {
+      super(name);
+      // if (this.roleMatches(role)) {
+      //   this.role = role;
+      // } else {
+      //   this.role = this.ROLES[0];
+      // }
+  
+      this.inventory.push("bedroll", "50 gold coins");
+    }
+  
+    scout() {
+      console.log(`${this.name} is scouting ahead...`);
+      super.roll();
+      console.log(this.ROLES);
+    }
+  
+    roleMatches(role) {
+      const result = false;
+      this.ROLES.forEach((element) => {
+        if (element === role) {
+          result = true;
+        }
+      });
+  
+      return result;
+    }
+  
+    duel(adventurer) {
+      const myroll = super.roll();
+      const enemyroll = adventurer.roll();
+      if (myroll > enemyroll) {
+        adventurer.health -= 1;
+        console.log(
+          `${this.name} rolled higher than ${adventurer.name}, ${adventurer.name} loses 1 health point`
+        );
+        console.log(`${adventurer.name} health is now ${adventurer.health}`);
+      } else {
+        this.health -= 1;
+        console.log(
+          `${adventurer.name} rolled higher than ${this.name}, ${this.name} loses 1 health point`
+        );
+        console.log(`${this.name} health is now ${this.health}`);
+      }
+    }
+  }
+  
+  class Companion extends Character {
+    constructor(name, type) {
+      super(name);
+  
+      this.type = type;
+    }
+  
+    assist() {
+      console.log(`${this.name} is helping `);
+      super.roll();
+    }
+  }
+  
+ 
